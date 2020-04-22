a351729a482c01e53393c7760c3ac6ad58db5a09
Dmitriy An <dmitriym44@gmail.com>
2020-04-23 01:15:56 +0200

    setup theme-ui theme

---
+++ src/gatsby-plugin-theme-ui.js
@@ -0,0 +1,121 @@
+export default {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fonts: {
+    body:
+      'Roboto, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", sans-serif',
+    heading:
+      'Poppins, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", sans-serif',
+    monospace: "Menlo, monospace",
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  colors: {
+    text: "#151515",
+    background: "#FAFAFA",
+    primary: "#0057FF",
+    // secondary: "#30c",
+    // muted: "#f6f6f6",
+    modes: {
+      dark: {
+        text: "#FFFFFF",
+        background: "#20232A",
+        primary: "#F8DF00",
+        // secondary: "#30c",
+        // muted: "#f6f6f6",
+      },
+    },
+  },
+  styles: {
+    root: {
+      fontFamily: "body",
+      lineHeight: "body",
+      fontWeight: "body",
+    },
+    h1: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 5,
+    },
+    h2: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 4,
+    },
+    h3: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 3,
+    },
+    h4: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 2,
+    },
+    h5: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 1,
+    },
+    h6: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 0,
+    },
+    p: {
+      color: "text",
+      fontFamily: "body",
+      fontWeight: "body",
+      lineHeight: "body",
+    },
+    a: {
+      color: "primary",
+    },
+    pre: {
+      fontFamily: "monospace",
+      overflowX: "auto",
+      code: {
+        color: "inherit",
+      },
+    },
+    code: {
+      fontFamily: "monospace",
+      fontSize: "inherit",
+    },
+    table: {
+      width: "100%",
+      borderCollapse: "separate",
+      borderSpacing: 0,
+    },
+    th: {
+      textAlign: "left",
+      borderBottomStyle: "solid",
+    },
+    td: {
+      textAlign: "left",
+      borderBottomStyle: "solid",
+    },
+    img: {
+      maxWidth: "100%",
+    },
+  },
+}
