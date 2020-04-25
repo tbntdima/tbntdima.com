9fe4803fc46e999b8dd3cec8b98be72eb17b76f0
Dmitriy An <dmitriym44@gmail.com>
2020-04-25 21:39:13 +0200

    chore: cleanup initial components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/shared/layout"
-import SEO from "../components/seo"
+import SEO from "../components/shared/seo"
 
 const IndexPage = () => (
   <Layout>
