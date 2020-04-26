1e51c36762a6e219a687ca98f4615bd469cd2fef
Dmitriy An <dmitriym44@gmail.com>
2020-04-26 17:05:41 +0200

    feat: remove projects from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import React from "react"
 import Layout from "../components/shared/layout"
 import SEO from "../components/shared/seo"
 import Bio from "../components/bio"
-import Projects from "../components/projects"
 import Blog from "../components/blog"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Bio />
-    <Projects />
     <Blog />
   </Layout>
 )
