e95d8fbd398899a915bac91b68a0576b343fe548
Dmitriy An <dmitriym44@gmail.com>
2020-04-26 17:13:40 +0200

    feat: update github and twitter buttons

--- src/components/githubButton.js
+++ src/components/githubButton.js
@@ -1,24 +1,47 @@
 /** @jsx jsx */
 import { jsx, Link } from "theme-ui"
 import { GrGithub } from "react-icons/gr"
+import { StaticQuery, graphql } from "gatsby"
 
 const GithubButton = () => (
-  <Link
-    href="https://github.com/tbntdima"
-    sx={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      textDecoration: "none",
-      background: "#EFF3F6",
-      border: "1px solid rgba(27, 31, 35, 0.2)",
-      borderRadius: "3px",
-      p: "6px 20px",
-      color: "#000",
-    }}
-  >
-    <GrGithub size="25px" sx={{ mr: 2 }} /> tbntdima
-  </Link>
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            socials {
+              github
+            }
+          }
+        }
+      }
+    `}
+    render={({
+      site: {
+        siteMetadata: {
+          socials: { github: githubUrl },
+        },
+      },
+    }) => (
+      <Link
+        href={githubUrl}
+        target="_blank"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textDecoration: "none",
+          background: "#EFF3F6",
+          border: "1px solid rgba(27, 31, 35, 0.2)",
+          borderRadius: "3px",
+          p: "6px 20px",
+          color: "#000",
+        }}
+      >
+        <GrGithub size="25px" sx={{ mr: 2 }} /> tbntdima
+      </Link>
+    )}
+  />
 )
 
 export default GithubButton
--- src/components/twitterButton.js
+++ src/components/twitterButton.js
@@ -1,24 +1,47 @@
 /** @jsx jsx */
 import { jsx, Link } from "theme-ui"
 import { GrTwitter } from "react-icons/gr"
+import { StaticQuery, graphql } from "gatsby"
 
 const Twitter = () => (
-  <Link
-    href="https://github.com/tbntdima"
-    sx={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      textDecoration: "none",
-      background: "#1FA1F3",
-      border: "1px solid #1FA1F3",
-      borderRadius: "3px",
-      p: "6px 20px",
-      color: "#fff",
-    }}
-  >
-    <GrTwitter size="25px" sx={{ mr: 2 }} /> @tbntdima
-  </Link>
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            socials {
+              twitter
+            }
+          }
+        }
+      }
+    `}
+    render={({
+      site: {
+        siteMetadata: {
+          socials: { twitter: twitterUrl },
+        },
+      },
+    }) => (
+      <Link
+        href={twitterUrl}
+        target="_blank"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textDecoration: "none",
+          background: "#1FA1F3",
+          border: "1px solid #1FA1F3",
+          borderRadius: "3px",
+          p: "6px 20px",
+          color: "#fff",
+        }}
+      >
+        <GrTwitter size="25px" sx={{ mr: 2 }} /> @tbnt_dima
+      </Link>
+    )}
+  />
 )
 
 export default Twitter
