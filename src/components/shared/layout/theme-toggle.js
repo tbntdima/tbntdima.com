412617425b33228b493e9e8e716c5dc035dd84d9
Dmitriy An <dmitriym44@gmail.com>
2020-04-28 02:01:16 +0200

    chore: add cursor pointer to theme toggle

--- src/components/shared/layout/theme-toggle.js
+++ src/components/shared/layout/theme-toggle.js
@@ -17,6 +17,7 @@ const ThemeToggle = () => {
         backgroundColor: "primary",
         borderRadius: 20,
         p: "2px",
+        cursor: "pointer",
       }}
       onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}
     >
